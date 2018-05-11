6df8892a25fa4c1e1dea22628c51de451eb3a3dc
JohnJohn <jferreira.ba@gmail.com>
2018-05-11 06:29:46 -0300

    Aula 02 - Finalizada

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -3,13 +3,20 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import br.com.caelum.livraria.modelo.Autor;
 
-
+/*
+ * Existe outros session bean além do Singleton e Stateless(SBSL) são eles:
+ * Stateful(SBSF) => É um objeto exclusivo de um cliente, apenas um cliente usará este objeto.
+ * 	O Session Bean Stateful (SBSF) tem uma funcionalidade muito parecida com a do objeto HttpSession: 
+ * 	representa um objeto para o cliente. Ideal para guardar informações que só dizem respeito ao cliente.
+ */
 @Stateless
+//@Stateful
 public class AutorDao {
 
 	@Inject
