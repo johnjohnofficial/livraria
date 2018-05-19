2848a05f8892f68825460eb3e729beac188a4ec5
JohnJohn <jferreira.ba@gmail.com>
2018-05-19 09:14:09 -0300

    Aula 07 - Integração com Web Services

--- src/br/com/caelum/livraria/dao/LivroDao.java
+++ src/br/com/caelum/livraria/dao/LivroDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.livraria.modelo.Livro;
 
@@ -24,6 +25,16 @@ public class LivroDao {
 	public List<Livro> todosLivros() {
 //		return banco.listaLivros();
 		return this.manager.createQuery("select l from Livro l", Livro.class).getResultList();
+	}
+
+	public List<Livro> getLivrosPorTitulo(String titulo) {
+		
+		String jpql = "select l from Livro l where l.titulo like :pTitulo";
+		
+		TypedQuery<Livro> query = this.manager.createQuery(jpql, Livro.class);
+		query.setParameter("pTitulo", "%" + titulo + "%");
+		
+		return query.getResultList();
 	}
 	
 }
---
+++ src/br/com/caelum/livraria/webservice/LivrariaWS.java
@@ -0,0 +1,29 @@
+package br.com.caelum.livraria.webservice;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import br.com.caelum.livraria.dao.LivroDao;
+import br.com.caelum.livraria.modelo.Livro;
+
+@WebService
+@Stateless
+public class LivrariaWS {
+	
+	@Inject
+	private LivroDao dao;
+	
+	@WebResult(name="Livro")
+	public List<Livro> getLivrosPorTitulo(@WebParam(name="nomeTitulo") String titulo) {
+		
+		System.out.println("[LivrariaWS] Procurando pelo titulo: " + titulo);
+		
+		return dao.getLivrosPorTitulo(titulo);
+	}
+
+}
